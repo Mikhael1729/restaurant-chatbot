fbda064faea6fa22bc03c172bea79818a61b49ea
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2021-04-25 00:46:24 -0400

    Print the messages and categories

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(2)
+	}
+}
 
 func main() {
-	fmt.Println("This is my chatbot :\"P\n---")
+	fmt.Println("This is my chatbot :\"P\n---\n")
+
+	file_data, err := ioutil.ReadFile("./training_data/chats")
+
+	if err != nil {
+		panic(err)
+	}
+
+	text := string(file_data)
+	all_responses := strings.Split(text, "#")
+
+	for _, message := range all_responses {
+		fmt.Println(message)
+	}
+}
+
+type CategoryMessages struct {
+	category string
+	messages []string
 }
