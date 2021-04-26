7610f742d55e7f260124b06084ecfc4d8526bb9d
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2021-04-25 22:57:49 -0400

    Use spaces instead of tabs

--- main.go
+++ main.go
@@ -1,51 +1,52 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
-	"strings"
+  "fmt"
+  "io/ioutil"
+  "strings"
 )
 
 func main() {
-	fmt.Println("This is my chatbot :\"P\n---")
+  fmt.Println("This is my chatbot :\"P\n---")
 
-	trainingData := extractTrainingResponses("./training_data/chats")
-	fmt.Println(trainingData)
+  trainingData := extractTrainingResponses("./training_data/chats")
+  fmt.Println(trainingData)
 }
 
 func extractTrainingResponses(filename string) map[string][]string {
-	file_data, err := ioutil.ReadFile(filename)
+  file_data, err := ioutil.ReadFile(filename)
 
-	if err != nil {
-		panic(err)
-	}
+  if err != nil {
+    panic(err)
+  }
 
-	// Stores the messages grouped by category.
-	grouped := make(map[string][]string)
+  // Stores the messages grouped by category.
+  grouped := make(map[string][]string)
 
-	// Remove null characters.
-	text := strings.Replace(string(file_data), "\x00", "", -1)
+  // Remove null characters.
+  text := strings.Replace(string(file_data), "\x00", "", -1)
 
-	// Separate the responses.
-	all_responses := strings.Split(text, "#")
+  // Separate the responses.
+  all_responses := strings.Split(text, "#")
 
-	// Group messages into categories.
-	for _, response := range all_responses {
-		parts := strings.Split(response, "(")
+  // Group messages into categories.
+  for _, response := range all_responses {
+    parts := strings.Split(response, "(")
 
-		if len(parts) <= 1 {
-			continue
-		}
+    if len(parts) <= 1 {
+      continue
+    }
 
-		message := parts[0]
-		category := parts[1][:len(parts[1])-1]
+    message := parts[0]
+    category := parts[1][:len(parts[1])-1]
 
-		if grouped[category] == nil {
-			grouped[category] = []string{message}
-		} else {
-			grouped[category] = append(grouped[category], message)
-		}
-	}
+    if grouped[category] == nil {
+      grouped[category] = []string{message}
+    } else {
+      grouped[category] = append(grouped[category], message)
+    }
+  }
 
-	return grouped
+  return grouped
 }
+
