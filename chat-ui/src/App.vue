d30677721165dff353734c55853e7d1ea626c301
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2021-05-20 17:11:29 -0400

    Place all components in the components directory

--- chat-ui/src/App.vue
+++ chat-ui/src/App.vue
@@ -1,28 +1,14 @@
 <template>
   <div class="container">
     <TitleDescription />
-    <div class="chat">
-      <ChatHistory />
-      <InputSection />
-    </div>
+    <Chat />
   </div>
 </template>
 
 <script setup>
-import InputSection from "./views/InputSection";
-import ChatHistory from "./views/ChatHistory";
-import TitleDescription from "./views/TitleDescription";
+import Chat from "./components/Chat"
+import TitleDescription from "./components/TitleDescription";
 </script>
-
-<style>
-* {
-  margin: 0;
-  padding: 0;
-  background-color: var(--surface-b);
-  font-family: var(--font-family);
-  color: var(--text-color);
-}
-</style>
 
 <style lang="scss" scoped>
 .container {
@@ -34,15 +20,4 @@ import TitleDescription from "./views/TitleDescription";
     margin-right: 3em;
   }
 }
-
-.chat {
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  padding: 2em;
-  border: 1px solid var(--text-color);
-  border-radius: 0.8em;
-  background: var(--surface-a);
-}
 </style>
-
---
+++ chat-ui/src/components/Chat.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="chat">
+    <ChatHistory />
+    <InputSection />
+  </div>
+</template>
+
+<script setup>
+import InputSection from "./ChatInput";
+import ChatHistory from "./ChatHistory";
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  background-color: var(--surface-b);
+  font-family: var(--font-family);
+  color: var(--text-color);
+}
+</style>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  padding: 3em;
+  height: 100vh;
+
+  & > *:not(:last-child) {
+    margin-right: 3em;
+  }
+}
+
+.chat {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  padding: 2em;
+  border: 1px solid var(--text-color);
+  border-radius: 0.8em;
+  background: var(--surface-a);
+}
+</style>
+
