666a867918310451b171cd57491517ce791dc43c
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2021-05-21 01:55:42 -0400

    Add post method for messages

--- chat-service/handlers/messages.go
+++ chat-service/handlers/messages.go
@@ -20,10 +20,16 @@ func (handler *Messages) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	if r.Method == http.MethodPost {
+		handler.addMessage(rw, r)
+		return
+	}
+
 	// Catch all.
 	rw.WriteHeader(http.StatusMethodNotAllowed)
 }
 
+// getMessages process the GET method for the handler
 func (m *Messages) getMessages(rw http.ResponseWriter, r *http.Request) {
 	messages := models.GetMessages()
 	err := messages.ToJson(rw)
@@ -31,4 +37,17 @@ func (m *Messages) getMessages(rw http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		http.Error(rw, "Unable to marshal messages json", http.StatusInternalServerError)
 	}
+}
+
+// getMessages process the POST method for the handler
+func (handler *Messages) addMessage(rw http.ResponseWriter, r *http.Request) {
+	message := &models.Message{}
+
+	err := message.FromJson(r.Body)
+
+	if err != nil {
+		http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
+	}
+
+	models.AddMessage(message)
 }
--- chat-service/models/message.go
+++ chat-service/models/message.go
@@ -17,46 +17,25 @@ const (
 type Message struct {
 	Id       int       `json:"id"`
 	Text     string    `json:"text"`
-	Sender   bool      `json:"sender"` // If true the sender is the human, otherwise the bot.
+	Sender   bool      `json:"sender,string"` // If true the sender is the human, otherwise the bot.
 	DateTime time.Time `json:"dateTime"`
 }
 
-type Messages []*Message
-
-func (messages *Messages) ToJson(writer io.Writer) error {
-	encoder := json.NewEncoder(writer)
-	encodedMessages := encoder.Encode(messages)
-	return encodedMessages
+func AddMessage(message *Message) {
+	message.Id = generateId()
+	mockMessages = append(mockMessages, message)
 }
 
-// GetMessages returns the list of stored messages of the chat.
-func GetMessages() Messages {
-	return mockMessages
+func generateId() int {
+	lastMessage := mockMessages[len(mockMessages)-1]
+	newId := lastMessage.Id + 1
+
+	return newId
 }
 
-var mockMessages = []*Message{
-	{
-		Id:       1,
-		Text:     "Hola",
-		Sender:   Customer,
-		DateTime: time.Now(),
-	},
-	{
-		Id:       2,
-		Text:     "Buenos días, ¿cómo se encuentra?",
-		Sender:   Bot,
-		DateTime: time.Now().Add(time.Minute * time.Duration(2)),
-	},
-	{
-		Id:       3,
-		Text:     "Muy bien, gracias",
-		Sender:   Customer,
-		DateTime: time.Now(),
-	},
-	{
-		Id:       4,
-		Text:     "¿Cómo le puedo ayudar?",
-		Sender:   Bot,
-		DateTime: time.Now().Add(time.Minute * time.Duration(2)),
-	},
+// FromJson converts a Message in JSON format to a Go Message struct.
+func (message *Message) FromJson(reader io.Reader) error {
+	decoder := json.NewDecoder(reader)
+	decoded := decoder.Decode(message)
+	return decoded
 }
