77d1296f9e258c74f1803830acf8a44c1553fd72
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2021-05-21 01:59:59 -0400

    Merge the content of messages with message and delete messages.go

--- chat-service/models/message.go
+++ chat-service/models/message.go
@@ -21,16 +21,24 @@ type Message struct {
 	DateTime time.Time `json:"dateTime"`
 }
 
+type Messages []*Message
+
+// GetMessages returns the list of stored messages of the chat.
+func GetMessages() Messages {
+	return mockMessages
+}
+
+// AddMessage adds a new Message to the database.
 func AddMessage(message *Message) {
 	message.Id = generateId()
 	mockMessages = append(mockMessages, message)
 }
 
-func generateId() int {
-	lastMessage := mockMessages[len(mockMessages)-1]
-	newId := lastMessage.Id + 1
-
-	return newId
+// ToJson converts a Messages into JSON format.
+func (messages *Messages) ToJson(writer io.Writer) error {
+	encoder := json.NewEncoder(writer)
+	encodedMessages := encoder.Encode(messages)
+	return encodedMessages
 }
 
 // FromJson converts a Message in JSON format to a Go Message struct.
@@ -38,4 +46,27 @@ func (message *Message) FromJson(reader io.Reader) error {
 	decoder := json.NewDecoder(reader)
 	decoded := decoder.Decode(message)
 	return decoded
+}
+
+// generateId generate a new Id for a new message.
+func generateId() int {
+	lastMessage := mockMessages[len(mockMessages)-1]
+	newId := lastMessage.Id + 1
+
+	return newId
+}
+
+var mockMessages = []*Message{
+	{
+		Id:       1,
+		Text:     "Hola",
+		Sender:   Customer,
+		DateTime: time.Now(),
+	},
+	{
+		Id:       2,
+		Text:     "Buenos días, ¿cómo se encuentra?",
+		Sender:   Bot,
+		DateTime: time.Now().Add(time.Minute * time.Duration(2)),
+	},
 }
