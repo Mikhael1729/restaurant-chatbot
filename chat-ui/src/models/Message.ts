eee517071ce6ef3294de5f5bba35ecacad1e4922
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2021-05-21 15:57:20 -0400

    Use numbers for Sender and make dateTime a string variable

--- chat-ui/src/models/Message.ts
+++ chat-ui/src/models/Message.ts
@@ -1,26 +1,29 @@
-type MessageType = "reciver" | "sender";
+export enum Sender {
+  Customer = 1,
+  Bot = 0,
+}
 
 export default class Message {
   id: string;
   text: string;
-  date: Date;
-  type: MessageType;
+  dateTime: string;
+  sender: Sender;
 
   constructor() {
     this.id = "";
     this.text = "";
-    this.date = new Date(Date.now());
-    this.type = "sender";
+    this.dateTime = "";
+    this.sender = Sender.Customer;
   }
 
   static mockData(): Message[] {
     const messages: Message[] = [];
     for (let i = 0; i < 12; i++) {
       const isEven = i % 2 === 0;
-      const type = !isEven ? "reciver" : "sender";
+      const sender = isEven ? Sender.Customer : Sender.Bot;
       const text = isEven ? "Hola, envío esto" : "Mi respuesta es: No >: 3";
 
-      messages.push({ id: `$m-{i}`, text, type, date: new Date(Date.now()) });
+      messages.push({ id: `$m-{i}`, text, sender, dateTime: "" });
     }
 
     return messages;
