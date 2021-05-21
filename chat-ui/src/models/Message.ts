54f2eb057e851a1173fddb7c5428daa7e45f7650
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2021-05-21 17:53:55 -0400

    Use a type instead of a class for Message

--- chat-ui/src/models/Message.ts
+++ chat-ui/src/models/Message.ts
@@ -3,29 +3,11 @@ export enum Sender {
   Bot = 1,
 }
 
-export default class Message {
+type Message = {
   id: string;
   text: string;
   dateTime: string;
   sender: Sender;
+};
 
-  constructor() {
-    this.id = "";
-    this.text = "";
-    this.dateTime = "";
-    this.sender = Sender.Customer;
-  }
-
-  static mockData(): Message[] {
-    const messages: Message[] = [];
-    for (let i = 0; i < 12; i++) {
-      const isEven = i % 2 === 0;
-      const sender = isEven ? Sender.Customer : Sender.Bot;
-      const text = isEven ? "Hola, envío esto" : "Mi respuesta es: No >: 3";
-
-      messages.push({ id: `$m-{i}`, text, sender, dateTime: "" });
-    }
-
-    return messages;
-  }
-}
+export default Message;
