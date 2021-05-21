c938d309925e9b5c236ba4f00ec52141f7e90a6a
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2021-05-20 23:26:34 -0400

    Add mock data of Message

--- chat-service/models/message.go
+++ chat-service/models/message.go
@@ -2,8 +2,43 @@ package service
 
 import "time"
 
+type Sender bool
+
+const (
+	Customer = true
+	Bot      = false
+)
+
 type Message struct {
+	Id       int
 	Text     string
 	Sender   bool // If true the sender is the human, otherwise the bot.
 	DateTime time.Time
+}
+
+var mockMessages = []*Message{
+	{
+		Id:       1,
+		Text:     "Hola",
+		Sender:   Customer,
+		DateTime: time.Now(),
+	},
+	{
+		Id:       2,
+		Text:     "Buenos días, ¿cómo se encuentra?",
+		Sender:   Bot,
+		DateTime: time.Now().Add(time.Minute * time.Duration(2)),
+	},
+	{
+		Id:       3,
+		Text:     "Muy bien, gracias",
+		Sender:   Customer,
+		DateTime: time.Now(),
+	},
+	{
+		Id:       4,
+		Text:     "¿Cómo le puedo ayudar?",
+		Sender:   Bot,
+		DateTime: time.Now().Add(time.Minute * time.Duration(2)),
+	},
 }
