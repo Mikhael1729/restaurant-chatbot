de90203c2356350a4b5b3f10a3950f73c55325c7
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2021-05-21 15:55:41 -0400

    Enable cors for all origins

--- chat-service/handlers/messages.go
+++ chat-service/handlers/messages.go
@@ -15,6 +15,7 @@ func NewMessages(logger *log.Logger) *Messages {
 }
 
 func (handler *Messages) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	setupCors(rw, r)
 	if r.Method == http.MethodGet {
 		handler.getMessages(rw, r)
 		return
@@ -50,4 +51,11 @@ func (handler *Messages) addMessage(rw http.ResponseWriter, r *http.Request) {
 	}
 
 	models.AddMessage(message)
+}
+
+func setupCors(rw http.ResponseWriter, req *http.Request) {
+	rw.Header().Set("Content-Type", "application/json")
+	rw.Header().Set("Access-Control-Allow-Origin", "*")
+	rw.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	rw.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 }
