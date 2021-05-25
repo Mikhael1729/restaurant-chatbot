8ad8bd3c85177816a487837ae813665122d8e922
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2021-05-25 14:29:47 -0400

    Change name of helpers.go

--- chat-service/ann/data_extraction.go
+++ chat-service/ann/data_extraction.go
@@ -7,6 +7,13 @@ import (
 	"strings"
 )
 
+type ExtractedData struct {
+	InputOptions  map[string]bool // Unique stemmed sentence words in all examples.
+	OutputOptions map[string]bool // Unique outputs (classes)
+	X             [][]string      // Stemmed words of all input sentences.
+	Y             []string        // The expected output of the examples X.
+}
+
 // normalizeWord stem the given word.
 func normalizeWord(word string) string {
 	stemmed, err := snowball.Stem(word, "spanish", true)
