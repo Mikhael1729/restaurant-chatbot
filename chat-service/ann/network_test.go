780cb7e53fba493b70f4ec5066d09f0c9a16f559
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2021-05-26 04:43:11 -0400

    Change name of TestPredictions

--- chat-service/ann/network_test.go
+++ chat-service/ann/network_test.go
@@ -1,18 +1,15 @@
 package ann
 
 import (
-	"fmt"
 	"gonum.org/v1/gonum/mat"
 	"testing"
 )
 
-func TestPredictions(t *testing.T) {
+func TestGetPredictions(t *testing.T) {
 	matrix := mat.NewDense(3, 2, []float64{2, 3, 3, 2, 1, 2})
 	predictions := getPredictions(matrix)
 
-	fmt.Println(predictions)
-
-	expectedData := []float64{1, 0, 1, 0}
+	expectedData := []float64{1, 0}
 	resultData := predictions.(*mat.Dense).RawMatrix().Data
 
 	perfectMatch, expected, result, i := Verify(expectedData, resultData)
